48351ead7e10b1a5658b462938b9efd1c45f96c4
AnonymousPlaysGR <142651157+AnonymousPlaysGR@users.noreply.github.com>
2023-10-20 20:00:09 +0300

    servers.js

---
+++ servers.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+  res.send('The Bot is Ready Now ┃  Please Follow me on Discord: apgr.#0840')
+})
+
+function server.js() {
+  server.listen(3000, () => {
+    console.log("Creator: AnonymousPlaysGR")
+  })
+}
+
+module.exports = keepAlive
